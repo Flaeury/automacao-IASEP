8d5fdd8ed1be3194d33ff6467d3699058624d1bc
flaeury <hi.alita1999@gmail.com>
2025-02-28 05:49:38 -0300

    adiciona modo de depuração ao servidor e ajusta títulos e estilos em templates HTML

--- app.py
+++ app.py
@@ -207,4 +207,4 @@ def delete_all_pdfs():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
